f649426e5817f3b3b436eb5cd706228262e406e6
Sandy Gomez <sgomez1029@gmail.com>
2024-04-09 21:23:34 -0700

    Initial Commit

---
+++ AddColorsToABarChart/AddColorsToABarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddColorsToABarChartApp.swift
+//  AddColorsToABarChart
+//
+//  Created by Sandra Gomez on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AddColorsToABarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
